0513dc235e7334813cb0f7781558bab234448fd9
Felix Gnass <fgnass@gmail.com>
2022-06-15 12:56:54 +0200

    chore: update capri version

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,6 @@
-import capri from "@capri-js/react";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import capri from "@capri-js/react/vite-plugin";
 
 export default defineConfig({
   plugins: [
