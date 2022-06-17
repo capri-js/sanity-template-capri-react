21b6bd1d145ad0f74f5f59dfd60638222668e69b
Felix Gnass <fgnass@gmail.com>
2022-06-17 22:35:32 +0200

    chore: change colors

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -1,13 +1,9 @@
 import styles from "./index.module.css";
-import github from "./github.svg";
 
 export function Footer() {
   return (
     <div className={styles.root}>
-      <a href="https://github.com/capri-js/capri">
-        <img src={github} width="24" height="24" alt="GitHub Logo" />
-        Visit Capri on GitHub
-      </a>
+      Made with <a href="https://capri.build/">capri.build</a>
     </div>
   );
 }
